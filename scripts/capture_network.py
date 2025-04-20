da14ac66cf577de3d920030325ccf944e58eba47
matis.codjia <matis.codjia@epita.fr>
2025-04-20 14:33:26 +0200

    feat: capture function

---
+++ scripts/capture_network.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def capture(interface='eth0', duration=60, output='../data/pcap/capture.pcap'):
+    print(f"Capturing for {duration}s on {interface}")
+    subprocess.run(["sudo", "timeout", str(duration), "tcpdump", "-i", interface, "-w", output])
+
+if __name__ == "__main__":
+    capture(interface="wlp1s0")
